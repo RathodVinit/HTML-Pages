6e252c2049e11cf11244c4b1af9ebc80ebd764bc
RathodVinit <rvinit4@gmail.com>
2022-02-12 00:25:27 +0530

    assignment 2

---
+++ WEB10/percentage.js
@@ -0,0 +1,6 @@
+var hindi=73;
+var maths=85;
+var english=62;
+
+percentage=((hindi+maths+english)*100)/300;
+console.log("percentage scored=",percentage,"%");
---
+++ WEB10/rectangle.js
@@ -0,0 +1,10 @@
+var length=17;
+var breadth=10;
+perimeter=(2*(length+breadth));
+console.log("perimeter of rectangle is=",perimeter,"cm");
+
+var lengthab=17;
+var breadthab=10;
+
+area=(lengthab*breadthab);
+console.log("area of rectangle is=",area,"cm")
