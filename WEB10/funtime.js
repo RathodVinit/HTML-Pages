9c4348157886b3b020825fe25e19d169f04774f1
RathodVinit <rvinit4@gmail.com>
2022-02-18 01:30:13 +0530

    camel case practice

---
+++ WEB10/funtime.js
@@ -0,0 +1,10 @@
+var viratKohli = "theAngryYoungMan";
+var mahendraSinghDhoni = "captainCool";
+var yuvrajSingh = "sixSixes";
+var sachinRAmeshTendulkar = "theMasterBlaster";
+var rahulSharadDravid = "theWall";
+var abrahamBenjaminDeVilliers = "360Degrees";
+var lasithMalinga = "yorkerSpecialist";
+var shoaibAkhtar = "fastestBallEver";
+var shaneWarne = "ballOfTheCentury";
+
---
+++ WEB10/problem.js
@@ -0,0 +1,13 @@
+// compound interest//
+var A = 0;//it is the annual amount;
+var P = 100000;//it is the pricipal amount//
+var R = 10;//this is the rate of interest//
+var N = 12;//no. of time interest applied//
+var T = 2;// time period//
+
+/* compound interest formula
+      is=  A=P(1+R/N)N*T
+*/
+formula= A = (P*(1+R/N)*(N*T))
+
+console.log(formula)
